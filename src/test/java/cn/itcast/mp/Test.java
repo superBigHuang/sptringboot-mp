ecdd71fc64a211e2a9127a7d244831f339d146f1
superBigHuang <841069102@qq.com>
2020-07-11 20:22:26 +0800

    通用枚举类型

---
+++ src/test/java/cn/itcast/mp/Test.java
@@ -0,0 +1,11 @@
+package cn.itcast.mp;
+
+import cn.itcast.mp.emuns.SexEnum;
+
+public class Test {
+
+    @org.junit.Test
+    public void aa(){
+        System.out.println(SexEnum.WOMAN.getValue());
+    }
+}

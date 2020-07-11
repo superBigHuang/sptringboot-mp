332a7f9f1332a30cd75e6d08f15c2dae33c122b4
superBigHuang <841069102@qq.com>
2020-07-11 13:32:00 +0800

    阻断全表更新删除插件的使用

--- src/main/java/cn/itcast/mp/MybatisPlusConfig.java
+++ src/main/java/cn/itcast/mp/MybatisPlusConfig.java
@@ -1,9 +1,17 @@
 package cn.itcast.mp;
 
+import cn.itcast.mp.plugins.MyInterceptor;
+import com.baomidou.mybatisplus.core.parser.ISqlParser;
+import com.baomidou.mybatisplus.extension.parsers.BlockAttackSqlParser;
 import com.baomidou.mybatisplus.extension.plugins.PaginationInterceptor;
+import com.baomidou.mybatisplus.extension.plugins.SqlExplainInterceptor;
+import org.apache.ibatis.jdbc.SQL;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
+import java.util.ArrayList;
+import java.util.List;
 
 // mybatis-plus 配置类
 @Configuration
@@ -12,7 +20,23 @@ public class MybatisPlusConfig {
 
     // 配置分页插件
     @Bean
-    public PaginationInterceptor paginationInterceptor(){
+    public PaginationInterceptor paginationInterceptor() {
         return new PaginationInterceptor();
+    }
+
+    // 注入自定义拦截器插件
+//    @Bean
+//    public MyInterceptor myInterceptor() {
+//        return new MyInterceptor();
+//    }
+
+    //
+    @Bean
+    public SqlExplainInterceptor sqlExplainInterceptor(){
+        SqlExplainInterceptor sqlExplainInterceptor = new SqlExplainInterceptor();
+        List<ISqlParser> list = new ArrayList();
+        list.add(new BlockAttackSqlParser()); // 全表更新或者删除的阻断器
+        sqlExplainInterceptor.setSqlParserList(list);
+        return sqlExplainInterceptor;
     }
 }
